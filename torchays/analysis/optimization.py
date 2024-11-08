a85c93fde11802ca1b16808bbc30b7eee0362b06
yui <yui_wy@outlook.com>
2024-11-09 01:30:50 +0800

    feat/master: add lineprog

--- torchays/analysis/optimization.py
+++ torchays/analysis/optimization.py
@@ -1,4 +1,5 @@
-from typing import Tuple
+from typing import Callable, Dict, Tuple
+
 import numpy as np
 from scipy import optimize
 
@@ -111,3 +112,62 @@ def minimize(
 ) -> Tuple[float, np.ndarray]:
     result = optimize.minimize(function, x0, method=method, constraints=constraints, jac=jac, tol=tol, options=options)
     return result.fun, result.x
+
+
+class Satisfy(Exception): ...
+
+
+def lineprog(
+    c: np.ndarray,
+    a_ub: np.ndarray,
+    b_ub: np.ndarray,
+    a_eq: np.ndarray = None,
+    b_eq: np.ndarray = None,
+    x0: np.ndarray = None,
+    method: str = None,
+    callback: Callable = None,
+    options: Dict = {
+        "maxiter": 100,
+        "ftol": 1e-16,
+    },
+):
+    return optimize.linprog(c, a_ub, b_ub, a_eq, b_eq, method=method, callback=callback, options=options, bounds=None, x0=x0)
+
+
+def cheby_ball(funcs: np.ndarray, x0: np.ndarray):
+
+    sol = lineprog(x0=x0)
+    return sol
+
+
+def _callback(x: np.ndarray, fun: float, success: bool, slack: np.ndarray, con: np.ndarray, phase: int, status: int, nit: int, message: str):
+    if fun >= 0 and slack.all() >= 0:
+        raise Satisfy()
+
+
+def lineprog_intersect(
+    func: np.ndarray,
+    a_ub: np.ndarray,
+    b_ub: np.ndarray,
+    x0: np.ndarray,
+) -> bool:
+    """
+    *   c @ x
+    *   A_ub @ x <= b_ub
+    *   x[-1] = c[-1]
+    """
+    c, lb = func[:-1], func[-1]
+    x0_r = c @ x0 + lb
+    if x0_r == 0:
+        return True
+    sign = 1 if x0_r > 0 else -1
+    a_eq = np.r_[np.zeros_like(func), 1]
+    a_eq = np.expand_dims(a_eq)
+    b_eq = lb
+    try:
+        sol = lineprog(func * sign, a_ub, b_ub, a_eq, b_eq, x0, callback=_callback)
+        if sol.fun <= 0 and sol.slack.all() >= 0:
+            return True
+        return False
+    except Satisfy:
+        return True
